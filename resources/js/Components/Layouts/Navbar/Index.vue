ed36043b4b96bc303fea3c26f9588f2f45ac91e0
Tata Adi Nugroho <tataa2237@gmail.com>
2024-05-24 01:29:57 +0700

    format code

--- resources/js/Components/Layouts/Navbar/Index.vue
+++ resources/js/Components/Layouts/Navbar/Index.vue
@@ -6,13 +6,22 @@
     <nav class="navbar navbar-expand-lg main-navbar">
         <form class="form-inline mr-auto">
             <ul class="navbar-nav mr-3">
-                <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
-                <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i
-                            class="fas fa-search"></i></a></li>
+                <li>
+                    <a href="#" data-toggle="sidebar" class="nav-link nav-link-lg">
+                        <i class="fas fa-bars"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none">
+                        <i class="fas fa-search"></i>
+                    </a>
+                </li>
             </ul>
             <div class="search-element">
                 <input class="form-control" type="search" placeholder="Search" aria-label="Search" data-width="250">
-                <button class="btn" type="submit"><i class="fas fa-search"></i></button>
+                <button class="btn" type="submit">
+                    <i class="fas fa-search"></i>
+                </button>
                 <div class="search-backdrop"></div>
                 <div class="search-result">
                     <div class="search-header">
@@ -204,8 +213,8 @@
                     </div>
                 </div>
             </li>
-            <li class="dropdown"><a href="#" data-toggle="dropdown"
-                    class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+            <li class="dropdown">
+                <a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
                     <img alt="image" src="img/avatar/avatar-1.png" class="rounded-circle mr-1">
                     <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div>
                 </a>

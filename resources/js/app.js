63b68532c99d328b06744e462b67eecc2b0b4fc4
Tata Adi Nugroho <tataa2237@gmail.com>
2024-05-25 10:23:23 +0700

    replace from vue create app to intertia create app

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,20 @@
 import './bootstrap';
-import {createApp} from 'vue'
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-import App from './App.vue'
-
-createApp(App).mount("#app")
+createInertiaApp({
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob('./Pages/**/*.vue')
+        ),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .mount(el)
+    },
+    progress: {
+        color: '#e91e63',
+    },
+});

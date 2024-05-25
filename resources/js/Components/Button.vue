70ca20d875c84cd0498cd5cefe08940f33df5303
Tata Adi Nugroho <tataa2237@gmail.com>
2024-05-25 10:27:00 +0700

    add button : vue components

---
+++ resources/js/Components/Button.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+    type: {
+        type: String,
+        default: "",
+    },
+    title: {
+        type: String,
+        default: "",
+    }
+});
+</script>
+<template>
+    <button type="submit" :class="`btn btn-${type} btn-lg btn-block`">
+        {{ title }}
+    </button>
+</template>

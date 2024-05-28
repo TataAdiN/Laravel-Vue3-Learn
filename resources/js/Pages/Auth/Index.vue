f862b3051e6004ab801bae153ed4ae154891300e
Tata Adi Nugroho <tataa2237@gmail.com>
2024-05-28 19:57:41 +0700

    replace html to vue components

--- resources/js/Pages/Auth/Index.vue
+++ resources/js/Pages/Auth/Index.vue
@@ -1,5 +1,8 @@
 <script setup>
+import RememberMe from './RememberMe.vue'
 import Button from "@/Components/Button.vue";
+import LoginSocial from './LoginSocial.vue';
+import Password from '../../Components/Form/Password.vue';
 </script>
 
 <template>
@@ -24,48 +27,13 @@ import Button from "@/Components/Button.vue";
 										Please fill in your email
 									</div>
 								</div>
-								<div class="form-group">
-									<div class="d-block">
-										<label for="password" class="control-label">Password</label>
-										<div class="float-right">
-											<a href="auth-forgot-password.html" class="text-small">
-												Forgot Password?
-											</a>
-										</div>
-									</div>
-									<input id="password" type="password" class="form-control" name="password"
-										tabindex="2" required>
-									<div class="invalid-feedback">
-										please fill in your password
-									</div>
-								</div>
-
-								<div class="form-group">
-									<div class="custom-control custom-checkbox">
-										<input type="checkbox" name="remember" class="custom-control-input" tabindex="3"
-											id="remember-me">
-										<label class="custom-control-label" for="remember-me">Remember Me</label>
-									</div>
-								</div>
+								<Password :title="'Password'" :showForgot="true"/>
+								<RememberMe />
 								<div class="form-group">
 									<Button :title="'Login'" :type="'primary'" tabindex="4"/>
 								</div>
 							</form>
-							<div class="text-center mt-4 mb-3">
-								<div class="text-job text-muted">Login With Social</div>
-							</div>
-							<div class="row sm-gutters">
-								<div class="col-6">
-									<a class="btn btn-block btn-social btn-facebook">
-										<span class="fab fa-facebook"></span> Facebook
-									</a>
-								</div>
-								<div class="col-6">
-									<a class="btn btn-block btn-social btn-twitter">
-										<span class="fab fa-twitter"></span> Twitter
-									</a>
-								</div>
-							</div>
+							<LoginSocial />
 						</div>
 					</div>
 					<div class="mt-5 text-muted text-center">

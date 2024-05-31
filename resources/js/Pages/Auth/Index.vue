6d8a77073bf6b1ea7c0c64acb4a9ba9a12a6dde1
Tata Adi Nugroho <tataa2237@gmail.com>
2024-05-31 16:00:18 +0700

    improve auth vue

--- resources/js/Pages/Auth/Index.vue
+++ resources/js/Pages/Auth/Index.vue
@@ -1,9 +1,9 @@
 <script setup>
-import RememberMe from './RememberMe.vue'
 import Button from "@/Components/Button.vue";
-import LoginSocial from './LoginSocial.vue';
-import Password from '../../Components/Form/Password.vue';
+import Password from '@/Components/Form/Password.vue';
 import TextField from "@/Components/Form/TextField.vue";
+import RememberMe from '@/Pages/Auth/RememberMe.vue'
+import LoginSocial from '@/Pages/Auth/LoginSocial.vue';
 </script>
 
 <template>
@@ -12,7 +12,8 @@ import TextField from "@/Components/Form/TextField.vue";
 			<div class="row">
 				<div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
 					<div class="login-brand">
-						<img src="img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle">
+						<img src="img/stisla-fill.svg" alt="logo" width="100" 
+							class="shadow-light rounded-circle">
 					</div>
 					<div class="card card-primary">
 						<div class="card-header">
@@ -20,11 +21,17 @@ import TextField from "@/Components/Form/TextField.vue";
 						</div>
 						<div class="card-body">
 							<form method="POST" action="#" class="needs-validation">
-                                <TextField :title="'Email'" :type="'email'"/>
-								<Password :title="'Password'" :showForgot="true"/>
+                                <TextField :title="'Email'" 
+									:type="'email'" 
+									:name="'email'"/>
+									:name="'email'"/>
+									:name="'email'"/>
+								<Password :title="'Password'" 
+									:showForgot="true"/>
 								<RememberMe />
 								<div class="form-group">
-									<Button :title="'Login'" :type="'primary'" tabindex="4"/>
+									<Button :title="'Login'" 
+										:type="'primary'"/>
 								</div>
 							</form>
 							<LoginSocial />
